0084ecc873049b67590e101d8c03df21be01e94a
ganesh <saiganeshrao01@gmail.com>
2025-12-05 17:30:39 +0530

    proper commitv1

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+
+import java.util.Arrays;
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] a = {1,2,3,45,6};
+
+        Arrays.sort(a);
+        System.out.print(Arrays.toString(a));  // while trying to print the array of numbers you will alsways need to conevert into string //
+    }}
+
+
+
+
+
