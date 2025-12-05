e8d33809e70123ac72cd14b23bb8318d856ba094
ganesh <saiganeshrao01@gmail.com>
2025-12-06 00:03:15 +0530

    Delete comit 1.4

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int[] a = {1,3,3,2,4,5,6};
+        int[] a = {1,3,3,2,4,4,5,6};
 
         Arrays.sort(a);
         System.out.print(Arrays.toString(a));  // while trying to print the array of numbers you will alsways need to conevert into string //
