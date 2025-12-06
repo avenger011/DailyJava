77a396c555a7ebdec7045c3a9588bb2d6243bc05
ganesh <saiganeshrao01@gmail.com>
2025-12-06 17:01:23 +0530

    second largest in and array(using array sort)

---
+++ java2/src/Main.java
@@ -0,0 +1,23 @@
+import static java.util.Arrays.sort;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+    int[] arr = {100,20,1,2,3,4,5,7,101};
+    int largest = 0;
+    int Slargest = 0;
+
+    Arrays.sort(arr);
+
+    for (int i=0; i<arr.length ; i++) {
+        if (arr[i] > largest) {
+            Slargest = largest;
+            largest = arr[i];
+        }
+
+    }
+    System.out.println(Slargest);
+
+
+
+}
