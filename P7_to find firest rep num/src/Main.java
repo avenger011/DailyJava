380badb3d576fe98e9be96453785db52c4d3ccbe
ganesh <saiganeshrao01@gmail.com>
2025-12-09 16:43:57 +0530

    p7 find first repeated number

---
+++ P7_to find firest rep num/src/Main.java
@@ -0,0 +1,18 @@
+//to find the first repested number in aerrsy without using hashset
+void main() {
+    int[] arr = {1, 2, 3, 4, 8, 3, 4, 5,0};
+    int a=0;
+
+    for (int i = 0; i < arr.length; i++) {
+        if(a==0){
+
+        for (int j = i + 1; j < arr.length; j++)
+        {
+            if(arr[i]==arr[j])
+            { System.out.println(arr[j]);
+            a=1;}
+
+        }
+
+    }}
+}
