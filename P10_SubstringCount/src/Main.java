8efe859f182423398daaea4badd533454f0db303
ganesh <saiganeshrao01@gmail.com>
2025-12-13 12:11:43 +0530

    p10 count of the substring in string

---
+++ P10_SubstringCount/src/Main.java
@@ -0,0 +1,17 @@
+//to find the count of the substring in string ,here "@#"
+void main() {
+
+    String str = "@#8765@#4567@#@#34567@#0987";
+   int index=0;
+   int count=0;
+
+    while((index=str.indexOf("@#",index))!=-1){
+        index=index+2;
+        count++;
+    }
+    System.out.println(count);
+}
+
+
+
+
