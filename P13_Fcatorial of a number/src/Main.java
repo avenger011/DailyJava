c26c7259a1c7ad3537d58693a7f5e43ebf94f7d8
ganesh <saiganeshrao01@gmail.com>
2025-12-14 13:58:57 +0530

    p13 Fcatorial of a number

---
+++ P13_Fcatorial of a number/src/Main.java
@@ -0,0 +1,15 @@
+//Fcatorial of a number
+void main() {
+//Fcatorial of a number
+    int num=10;
+    int a1=0;
+    int a2=1;
+    if(num>=0){System.out.print(a1+" ");}
+    if(num>=1){System.out.print(a1+" ");}
+    for(int i=1;i<=num;i++)
+    {
+        System.out.print(a1+a2+" ");
+        a1=a2;
+        a2=i;
+    }
+}
