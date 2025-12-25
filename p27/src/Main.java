013707ab1cd468711fe78750cf9216fa86e4ea44
ganesh <saiganeshrao01@gmail.com>
2025-12-25 19:01:42 +0530

    7

---
+++ p27/src/Main.java
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+String str1="asertt";
+String str2="asert";
+//to check if two strings are same using ternary operator;
+
+String isit=(str1.equals(str2))?"oh yeshh":"nopee";          //(conditrion)?if yes:if no
+System.out.println(isit);
+
+
+
+
+
+
+
+
+}
